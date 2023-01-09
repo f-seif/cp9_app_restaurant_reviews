4670c61adcdacc02446bc3a25b4dd0b521c5f088
f-seif <f.seif@yandex.com>
2023-01-09 23:43:51 +0100

    s

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import app from "./server.js"
-import mongodb from "mongodb"
-import dotenv from "dotenv"
-import RestaurantsDAO from "./dao/restaurantsDAO.js"
-import ReviewsDAO from "./dao/reviewsDAO.js"
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import RestaurantsDAO from "./dao/restaurantsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 //
