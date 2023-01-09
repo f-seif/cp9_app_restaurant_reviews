5cded198e9b84ab0715581f42673848d1f5aad5b
f-seif <f.seif@yandex.com>
2023-01-09 23:26:54 +0100

    a

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 import app from "./server.js"
-import mongoose from "mongoose"
+import mongodb from "mongodb"
 import dotenv from "dotenv"
 import RestaurantsDAO from "./dao/restaurantsDAO.js"
 import ReviewsDAO from "./dao/reviewsDAO.js"
 dotenv.config()
-//const MongoClient = mongodb.MongoClient
+const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8000
 
-mongoose.connect(
+MongoClient.connect(
   process.env.RESTREVIEWS_DB_URI,
     {
         maxPoolSize: 50,
