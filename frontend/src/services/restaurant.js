c930838c975f8bcf7d5485551d2eb456fd54f38a
f-seif <f.seif@yandex.com>
2023-01-10 21:19:40 +0100

    url

--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -1,32 +1,34 @@
 import http from "../http-common";
 
+const url = 'https://cp9-app-restaurant-reviews.vercel.app/';
+
 class RestaurantDataService {
   getAll(page = 0) {
-    return http.get(`restaurants?page=${page}`);
+    return http.get(url + `restaurants?page=${page}`);
   }
 
   get(id) {
-    return http.get(`/restaurant?id=${id}`);
+    return http.get(url + `/restaurant?id=${id}`);
   }
 
   find(query, by = "name", page = 0) {
-    return http.get(`restaurants?${by}=${query}&page=${page}`);
-  } 
+    return http.get(url + `restaurants?${by}=${query}&page=${page}`);
+  }
 
   createReview(data) {
-    return http.post("/review-new", data);
+    return http.post(url + "/review-new", data);
   }
 
   updateReview(data) {
-    return http.put("/review-edit", data);
+    return http.put(url + "/review-edit", data);
   }
 
   deleteReview(id, userId) {
-    return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
+    return http.delete(url + `/review-delete?id=${id}`, {data:{user_id: userId}});
   }
 
   getCuisines(id) {
-    return http.get(`/cuisines`);
+    return http.get(url + `/cuisines`);
   }
 
 }
